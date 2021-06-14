e055170c4ab6bd9a76e036b2b419e93cb6c7f1a9
Fabio-Coder <0xfab1015@gmail.com>
2021-06-13 22:51:55 -0300

    mais testes

--- test_exercicios/test_exercicios_basicos.py
+++ test_exercicios/test_exercicios_basicos.py
@@ -1,4 +1,6 @@
 import pytest
+
+from Exercícios.maior_e_menor_de_3 import menor_de_3, maior_de_3
 from Exercícios.romeu_e_julieta import romeu_e_julieta
 
 
@@ -7,3 +9,9 @@ def test_romeu_e_julieta_retorna_queijo():
     esperado = 'Queijo'
     resultado = romeu_e_julieta(entrada)
     assert resultado == esperado
+
+def test_menor_de_3_retorna_o_menor_valor():
+    assert menor_de_3(3,1,2) == 1
+
+def test_maior_de_3_retorna_o_maior_valor():
+    assert maior_de_3(3,1,2) == 3
